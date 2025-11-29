ec16306afc075b2d2e10ef9d8fc87c40609fb55a
michaltramita <michaltramita@gmail.com>
2025-11-30 00:56:47 +0100

    Update main.jsx

--- kod/src/main.jsx
+++ kod/src/main.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import '@/index.css';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/SupabaseAuthContext';
 import { Toaster } from './components/ui/toaster';
 
 const Root = () => (
   <>
-    <HashRouter>
+    <BrowserRouter>
       <AuthProvider>
         <App />
         <Toaster />
       </AuthProvider>
-    </HashRouter>
+    </BrowserRouter>
   </>
 );
 
